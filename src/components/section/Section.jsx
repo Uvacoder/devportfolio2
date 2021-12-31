c73e0efda9f4fde2e884daa573407df6ed086b9c
codetann <thetannerthomas@gmail.com>
2021-12-30 23:10:04 -0700

    minor change

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -2,7 +2,7 @@ import { VStack } from "@chakra-ui/react";
 
 export function Section({ children, align = "left", id }) {
   return (
-    <VStack id={id} as="section" w="4xl" align={align} spacing={0}>
+    <VStack id={id} as="section" w="full" maxW="4xl" align={align} spacing={0}>
       {children}
     </VStack>
   );
